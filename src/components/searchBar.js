d846efef02e01dc023795db523577594d318e83f
PinChun-Kuo <pinery201@gmail.com>
2020-09-04 15:34:54 +0800

    Handle invalid search input

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+const repoNameRule = /^[A-Za-z0-9\-_]+$/;
+
 const SearchBar = ({ classes, inputRef, inputClasses, updateQuery }) => {
   const [keyword, setKeyword] = useState('');
+  const [error, setError] = useState('');
   const updateKeyword = () => { setKeyword(inputRef.current.value) }
   const handleChange = () => {
     updateKeyword();
     updateQuery();
+
+    const isValid = inputRef.current.value.match(repoNameRule);
+
+    if (isValid) {
+      setError('');
+    } else {
+      setError('Please enter a valid repository name.')
+    }
   }
 
   return (
@@ -15,11 +26,12 @@ const SearchBar = ({ classes, inputRef, inputClasses, updateQuery }) => {
       <input
         ref={inputRef}
         value={keyword}
-        className={classNames('search-input', inputClasses)}
+        className={classNames('search-input', inputClasses, { 'has-error': !!error })}
         type='text'
         placeholder='Please enter search keyword'
         onChange={handleChange}
       />
+      { error && ( <span className='error-msg'>{error}</span> ) }
     </div>
   )
 }
